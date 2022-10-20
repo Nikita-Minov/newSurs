53b825376976142cfa5f999e2e572bf460faa510
Nikita-Minov <nikitachryackhov555@gmail.com>
2022-10-20 16:33:01 +0300

    Fixes

--- robloxweb/routers/api.js
+++ robloxweb/routers/api.js
@@ -102,16 +102,17 @@ router.post("/can_buy", async (req, res) => {
 
 			res.status(200).json({ ok: true, orderId, link: "https://payok.io/pay?" + querystring.encode(data) });
 		} else if(req.body.payType === "gamemoney") {
+			console.log("IP: " + req.ip);
 			const hash = crypto.createHmac('sha256', 'q7gJzVz2l664lzqe782YOMg8iUWpdp0a')
 
 				// updating data
 				.update(
-					`amount:${(parseInt(req.body.sum) / cfg.exchangeRate).toFixed(2)};ip:${req.body.ip};project:322;project_invoice:${orderId};type:qiwi;user:${req.body.username};wallet:G53203729768;`
+					`amount:${(parseInt(req.body.sum) / cfg.exchangeRate).toFixed(2)};ip:${req.ip};project:322;project_invoice:${orderId};type:qiwi;user:${req.body.username};wallet:G53203729768;`
 				)
 
 				// Encoding to be used
 				.digest('hex');
-			const dataParams = `project=322&type=qiwi&user=${req.body.username}&ip=${req.body.ip}&amount=${(parseInt(req.body.sum) / cfg.exchangeRate).toFixed(2)}&wallet=G53203729768&project_invoice=${orderId}&signature=${hash}`;
+			const dataParams = `project=322&type=qiwi&user=${req.body.username}&ip=${req.ip}&amount=${(parseInt(req.body.sum) / cfg.exchangeRate).toFixed(2)}&wallet=G53203729768&project_invoice=${orderId}&signature=${hash}`;
 			console.log(dataParams);
 
 		 	await axios.post('https://paygate.gamemoney.com/terminal/create', dataParams)
